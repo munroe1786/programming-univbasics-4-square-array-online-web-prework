311d9818d0658dcde32cdc0d7cfbd7e70fadf63e
Eliza Munroe <era3munroe@gmail.com>
2019-07-01 12:34:09 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,8 +1,10 @@
 def square_array(array)
-  counter=0
-  new_numbers=[]
-  while array [counter]do
-    square_array [counter]**2
+  counter = 0 
+  
+  while array[counter] do
+    new_array = array[counter] ** 2 
     counter += 1
+    return new_array
+    
   end
 end
